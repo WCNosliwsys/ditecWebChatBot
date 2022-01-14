bc608cabbbe1b51d63a7f3d22df3308099865a1a
Wilson Callisaya <demarktec@gmail.com>
2022-01-13 20:32:51 -0500

    enviando peticion socket.io

--- js/form.js
+++ js/form.js
@@ -4,46 +4,50 @@ var token='';
 let lbltelefono;
 let inputNombre;
 let ubicacion;
+let milatlng;
 window.addEventListener("load",function(){
   ubicacion=document.getElementById("coordenada");
+  var socket = io('http://ditec.eu-4.evennode.com/users');
+  socket.on('connected', function (data) {
+
+    console.log('connected');
+    socket.emit('signin','operador@',function(cbmsg,sckt){
+      console.log(cbmsg+ "socketdi: "+ sckt);
+    });
+    console.log(socket.id);
+  });
+
 
   document.querySelector("#submit").addEventListener("click", e => {
     e.preventDefault();
   
-    //INGRESE UN NUMERO DE WHATSAPP VALIDO AQUI:
-    let telefono = "573105010573";
-  
-    let cliente = document.querySelector("#cliente").value;
-    let servicio = document.querySelector("#servicio").value;
-    let resp = document.querySelector("#respuesta");
-  
-    resp.classList.remove("fail");
-    resp.classList.remove("send");
-  
-    let url = `https://api.whatsapp.com/send?phone=${telefono}&text=
-      *_MI NEGOCIO_*%0A
-      *Reservas*%0A%0A
-      *¿Cuál es tu nombre?*%0A
-      ${cliente}%0A
-      *Indica la fecha de tu reserva*%0A
-      ${fecha}%0A
-      *Indica la hora de tu reserva*%0A
-      ${hora}%0A
-      *Empleado de preferencia*%0A
-      ${empleado}%0A
-      *¿Cuál es el servicio que se desea realizar?*%0A
-      ${servicio}`;
-  
-    if (cliente === "" || fecha === "" || hora === "") {
+/*     if (cliente === "" || fecha === "" || hora === "") {
       resp.classList.add("fail");
       resp.innerHTML = `Faltan algunos datos, ${cliente}`;
       return false;
-    }
-    resp.classList.remove("fail");
-    resp.classList.add("send");
+    } */
+
     resp.innerHTML = `Se ha enviado tu reserva, ${cliente}`;
-  
-    window.open(url);
+    var data = {};
+    data.latitude = milatlng.lat();
+    data.longitude = milatlng.lng();;
+    data.email = 'operador@';
+    data.user="Whatsapp"
+    data.phone=phone;
+    data.name=inputNombre.value;
+    data.note = document.getElementById('referencia').value;
+    data.address = document.getElementById('direccion').value;
+    data.tipomovil= "Cualquiera";
+    data.zona="";
+    data.tipopeticion="normal"; 
+    data.price='';
+    data.to="";
+    data.exactprice="";
+
+    console.log(data);
+
+/*     socket.emit('new request',data,function(cb){
+      console.log("se ejecuto el new request");}); */
   });
 
   console.log("probando inicio");
@@ -108,6 +112,7 @@ function putmarker(latLng, map,clic) {
   direccion=direccion.substring(1, direccion.length-1);
   callWebService(direccion,clic);*/
   ubicacion.innerHTML='latlng: '+ latLng
+  milatlng=latLng;
 /*       if(clic==1)
         realizaractivate();
       else
